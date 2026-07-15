eeb4ab4240fec5c83dc017f8198ebee93ff78668
ravikhokle <ravikhokle1@gmail.com>
2026-07-15 15:40:58 +0530

    data types

---
+++ dataType.py
@@ -0,0 +1,20 @@
+vector = [1, 2, 3]
+
+matrix = [
+    [1, 2],
+    [3, 4]
+]
+
+my_list = ["Python", 100, True]
+
+data = {
+    "Vector": vector,
+    "Matrix": matrix,
+    "List": my_list
+}
+
+print("First Element:")
+print(data["Vector"])
+
+print("\nSecond Element:")
+print(data["Matrix"])
