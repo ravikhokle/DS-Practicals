1cbe11e8ccdb81f0288ba4946b246504188cafc4
ravikhokle <ravikhokle1@gmail.com>
2026-07-15 15:30:08 +0530

    vector practical

---
+++ vector.py
@@ -0,0 +1,14 @@
+a = [1, 2, 3]
+b = [3, 4, 5]
+c = [7, 8, 9]
+
+matrix = [
+    [a[0], b[0], c[0]],
+    [a[1], b[1], c[1]],
+    [a[2], b[2], c[2]]
+]
+
+print("Matrix")
+
+for row in matrix:
+    print(row)
